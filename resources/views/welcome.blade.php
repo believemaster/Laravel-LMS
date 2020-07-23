c63180971c6c4b0e56dde1e52cae6912bebf0b09
Yanik Kumar <believemasters@gmail.com>
2020-07-24 01:58:58 +0530

    Login form with vuejs and axios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+{{-- <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -96,4 +96,59 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
+
+@extends('layouts.app')
+
+@section('header')
+    <!-- Header -->
+    <header class="header header-inverse h-fullscreen pb-80" data-parallax="{{ asset('assets/img/bg-man.jpg') }}" data-overlay="8">
+        <div class="container text-center">
+
+          <div class="row h-full">
+            <div class="col-12 col-lg-8 offset-lg-2 align-self-center">
+
+              <h1 class="display-4 hidden-sm-down">Believe Master Magic</h1>
+              <h1 class="hidden-md-up">A place you will find visuals</h1>
+              <br>
+              <p class="lead text-white fs-20 hidden-sm-down"><span class="fw-400">The</span> place where we help you<br> teach you and inspire you by creating <span class="mark-border">Visuals</span>.</p>
+
+              <br><br><br>
+
+              <a class="btn btn-lg btn-round w-200 btn-primary mr-16" href="#" data-scrollto="section-intro">Demos</a>
+              <a class="btn btn-lg btn-round w-200 btn-outline btn-white hidden-sm-down" href="#" data-scrollto="section-intro">Features</a>
+
+            </div>
+
+            <div class="col-12 align-self-end text-center">
+              <a class="scroll-down-1 scroll-down-inverse" href="#" data-scrollto="section-intro"><span></span></a>
+            </div>
+
+          </div>
+
+        </div>
+      </header>
+    <!-- END Header -->
+@endsection
+
+@section('content')
+<section class="section" id="section-intro">
+    <div class="container">
+      <header class="section-header">
+        <small>Lessons</small>
+        <h2>Featured</h2>
+        <hr>
+        <p class="lead">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      </header>
+
+
+      <div class="row gap-y">
+
+
+      </div>
+
+
+    </div>
+  </section>
+@endsection
+
