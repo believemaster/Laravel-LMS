8c21e07d5e9e88e9df158ca5bedf3959c25e4880
Yanik Kumar <believemasters@gmail.com>
2020-08-07 06:51:39 +0530

    Completed Lessons for series

--- app/Entities/Learning.php
+++ app/Entities/Learning.php
@@ -2,6 +2,7 @@
 
 namespace App\Entities;
 
+use App\Lesson;
 use Illuminate\Support\Facades\Redis;
 
 trait Learning
@@ -21,6 +22,25 @@ trait Learning
 
     public function getNumberOfCompletedLessonsForASeries($series)
     {
-        return count(Redis::smemebers("user:{$this->id}:series:{$series->id}"));
+        return count($this->getCompletedLessonsForASeries($series));
+    }
+
+    public function getCompletedLessonsForASeries($series)
+    {
+        Redis::smemebers("user:{$this->id}:series:{$series->id}");
+    }
+
+    public function hasStarted($series)
+    {
+        return $this->getNumberOfCompletedLessonForASeries($series) > 0;
+    }
+
+    public function getCompletedSeries($series)
+    {
+        $completedLessons = $this->getCompletedLessonsForASeries($series);
+
+        return collect($completedLessons)->map(function ($lessonId) {
+            return Lesson::find($lessonId);
+        });
     }
 }
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -62,4 +62,50 @@ class UserTest extends TestCase
             50
         );
     }
+
+    public function test_can_know_if_a_user_has_started_a_series()
+    {
+        $this->flushRedis();
+
+        $user = factory(User::class)->create();
+        $lesson = factory(Lesson::class)->create();
+        $lesson2 = factory(Lesson::class)->create([
+            'series_id' => 1
+        ]);
+        $lesson3 = factory(Lesson::class)->create();
+
+        $user->completeLesson($lesson2);
+
+        $this->assertTrue($user->hasStartedSeries($lesson->series));
+        $this->assertFalse($user->hasStartedSeries($lesson3->series));
+    }
+
+    public function test_can_get_completed_lessons_for_a_series()
+    {
+        $this->flushRedis();
+
+        $user = factory(User::class)->create();
+        $lesson = factory(Lesson::class)->create();
+        $lesson2 = factory(Lesson::class)->create([
+            'series_id' => 1
+        ]);
+        $lesson3 = factory(Lesson::class)->create([
+            'series_id' => 1
+        ]);
+
+        $user->completeLesson($lesson);
+        $user->completeLesson($lesson2);
+
+        $completedLessons = $user->getCompletedLessons($lesson->series);
+
+        $this->assertInstanceOf(\Illuminate\Support\Collection::class, $completedLessons);
+
+        $this->assertInstanceOf(Lesson::class, $completedLessons->random());
+
+        $completedLessonsIds = $completedLessons->pluck('id')->all();
+
+        $this->assertTrue(in_array($lesson->id, $completedLessonsIds));
+        $this->assertTrue(in_array($lesson2->id, $completedLessonsIds));
+        $this->assertFalse(in_array($lesson3->id, $completedLessonsIds));
+    }
 }
